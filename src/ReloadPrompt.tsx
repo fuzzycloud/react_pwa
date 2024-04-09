759c0b9d1decc3e070f121776161cac117470614
Kunjan Dalal <kunjee17@gmail.com>
2024-04-09 10:17:36 +0530

    another minor change to get more info

--- src/ReloadPrompt.tsx
+++ src/ReloadPrompt.tsx
@@ -1,16 +1,34 @@
 import './ReloadPrompt.css'
 
 import { useRegisterSW } from 'virtual:pwa-register/react'
+import { pwaInfo } from 'virtual:pwa-info'
+
+console.log(pwaInfo)
 
 function ReloadPrompt() {
+  // replaced dynamically
+  const buildDate = '__DATE__'
+  // replaced dyanmicaly
+  const reloadSW = '__RELOAD_SW__'
+
   const {
     offlineReady: [offlineReady, setOfflineReady],
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
   } = useRegisterSW({
-    onRegistered(r) {
-      // eslint-disable-next-line prefer-template
-      console.log('SW Registered: ' + r)
+    onRegisteredSW(swUrl, r) {
+      console.log(`Service Worker at: ${swUrl}`)
+      // @ts-expect-error just ignore
+      if (reloadSW === 'true') {
+        r && setInterval(() => {
+          console.log('Checking for sw update')
+          r.update()
+        }, 20000 /* 20s for testing purposes */)
+      }
+      else {
+        // eslint-disable-next-line prefer-template
+        console.log('SW Registered: ' + r)
+      }
     },
     onRegisterError(error) {
       console.log('SW registration error', error)
@@ -25,17 +43,18 @@ function ReloadPrompt() {
   return (
     <div className="ReloadPrompt-container">
       { (offlineReady || needRefresh)
-        && <div className="ReloadPrompt-toast">
-            <div className="ReloadPrompt-message">
-              { offlineReady
-                ? <span>App ready to work offline</span>
-                : <span>New content available, click on reload button to update.</span>
-              }
-            </div>
-            { needRefresh && <button className="ReloadPrompt-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button> }
-            <button className="ReloadPrompt-toast-button" onClick={() => close()}>Close</button>
+      && (
+        <div className="ReloadPrompt-toast">
+          <div className="ReloadPrompt-message">
+            { offlineReady
+              ? <span>App ready to work offline</span>
+              : <span>New content available, click on reload button to update.</span>}
+          </div>
+          { needRefresh && <button className="ReloadPrompt-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button> }
+          <button className="ReloadPrompt-toast-button" onClick={() => close()}>Close</button>
         </div>
-      }
+      )}
+      <div className="ReloadPrompt-date">{buildDate}</div>
     </div>
   )
 }
